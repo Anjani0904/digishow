9edb4db01db10a731e8e5f08983ba924ac910079
Anjani0904 <98646105+Anjani0904@users.noreply.github.com>
2022-05-31 19:49:15 +0530

    first commit

---
+++ myproject/main/static/js/main.js
@@ -0,0 +1,5 @@
+const readMoreBtn=document.querySelector('.read-more-btn');
+const test=document.querySelector('.text');
+readMoreBtn.addEventListener('click',(e)=>{
+    Text.classList.toggle('show-more');
+})
